a2cc0cf284169b47dd394161bd5bc5b371e73bfa
MelloJudite <juditermello@gmail.com>
2026-03-13 10:41:06 -0300

    build: criando dicionario global db

--- test_api.py
+++ test_api.py
@@ -1,4 +1,14 @@
-from utils import *
-from views import *
 from servidor import *
 from unittest.mock import patch
+
+DICIONARIO_IMOVEIS = {
+            'id': int(1),
+            'logradouro': 'Nicole Common',
+            'tipo_logradouro': 'Travessa',
+            'bairro': 'Lake Danielle',
+            'cidade': 'Judymouth',
+            'cep': '85184',
+            'tipo': 'casa em condominio',
+            'valor': float(488424),
+            'data_aquisicao': '2017-07-29   ',
+        }
