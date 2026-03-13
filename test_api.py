2890c55bebc91ba55423c43f0702697ea089b5c0
MelloJudite <juditermello@gmail.com>
2026-03-13 10:50:38 -0300

    build: adicionando mais 1 dicionário global db

--- test_api.py
+++ test_api.py
@@ -1,7 +1,8 @@
 from servidor import *
 from unittest.mock import patch
 
-DICIONARIO_IMOVEIS = {
+DICIONARIO_IMOVEIS = [
+        {
             'id': int(1),
             'logradouro': 'Nicole Common',
             'tipo_logradouro': 'Travessa',
@@ -10,5 +11,17 @@ DICIONARIO_IMOVEIS = {
             'cep': '85184',
             'tipo': 'casa em condominio',
             'valor': float(488424),
-            'data_aquisicao': '2017-07-29   ',
+            'data_aquisicao': '2017-07-29',
+        },
+        {
+            'id': int(2),
+            'logradouro': 'Price Prairie',
+            'tipo_logradouro': 'Travessa',
+            'bairro': 'Colonton',
+            'cidade': 'North Garyville',
+            'cep': '93354',
+            'tipo': 'casa em condominio',
+            'valor': float(260070),
+            'data_aquisicao': '2021-11-30',  
         }
+]
